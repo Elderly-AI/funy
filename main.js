8a903d7d4d1a829bcbc99531666bd769c7065d90
Elderly-AI <56202288+Elderly-AI@users.noreply.github.com>
2020-09-12 20:07:44 +0300

    fix

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 document.getElementById("big-button").addEventListener("click", handler_a1);
 document.getElementById("big-button").addEventListener("mouseover", handler_a2);
+let playing = null;
 
 function handler_a1(e) {
+    if(playing){
+        playing.pause();
+    }
     (new Audio('a1.mp3')).play();
     document.getElementById("big-button").classList.add('hide');
     e.target.removeEventListener(e.type, arguments.callee);
 }
 
 function handler_a2(e) {
-    (new Audio('a2.mp3')).play();
+    playing = new Audio('a2.mp3');
+    playing.play();
     e.target.removeEventListener(e.type, arguments.callee);
 }
